95985be4b2a3d86cc08f2064ee60644a9b6c363d
michael <michaelchan_wahyan@yahoo.com.hk>
2021-01-06 00:28:12 +0800

    [release] release v1.8 for fixing NT Mak

---
+++ src/buildNtTex.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+python3 genNtTexSrc.py
+cd nt_out
+# 1st XeLaTex build, presuming
+# - failure in toc               if there does not have nt.toc
+# - failure in hyperref from toc if there does not have nt.aux
+#xelatex -syntax=1 nt.tex # will generate nt.toc , nt.aux
+# 2nd XeLaTex build, supposing
+# - success in toc               since there has nt.toc
+# - success in hyperref from toc since there has nt.aux
+xelatex -syntax=1 nt.tex #second build, with success in toc
+rm -f nt.maf nt.mtc* nt.out nt.log
+mv nt.pdf ../../
+# 3rd XeLaTex build, for small character version
+cp  nt.aux  nt_small.aux
+cp  nt.toc  nt_small.toc
+cat nt.tex | sed 's/\\large/\\small/' > nt_small.tex
+xelatex -syntax=1 nt_small.tex
+rm -f nt_small.maf nt_small.mtc* nt_small.out nt_small.log
+mv nt_small.pdf ../../
+cd ..
