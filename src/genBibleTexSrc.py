6c8239782aa7fecb9273bfd58720cfa7af6b1404
michael <michaelchan_wahyan@yahoo.com.hk>
2022-10-15 23:07:14 +0800

    [update] refactor genBible; fix NT Greek Punctuation

--- src/genBibleTexSrc.py
+++ src/genBibleTexSrc.py
@@ -449,85 +449,130 @@ for ntBook in nt_index :
                ( chapterIdx > 9  and int(content_cuv1[sentenceIdx].split(".")[0]) == chapterIdx ) :
                 bibleStr = "\\begin{tabularx}{\\textwidth}{|c|c||X|}\n"; fp.write( bibleStr )
                 bibleStr = "\hline\n"                                  ; fp.write( bibleStr )
+                # obtain chapter_verse string
+                ch_vsStr = content_cuv1[sentenceIdx].replace("\n","").split(" ",1)[0].replace(".",":")
+                # <<<< when a new version is added, argument in "multirow" requires adjustment >>>>
+                bibleStr = "\\multirow{11}{*}{\\rotatebox[origin=c]{90}{\\hfill "+words[1]+" "+words[3]+" $"+ch_vsStr+"$ \\hfill}}"
+                fp.write( bibleStr )
+                # ---------------------------------------------------
+                # add the content of ccvv to 1st row
+                # ---------------------------------------------------
+                bibleStr = content_ccvv[sentenceIdx].replace("\n","")
+                bibleStr = bibleStr.split(" ",1)
+                bibleStr = bibleStr[1]
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[0]+"!"+str(colorIntensity)+"}"+"\\scriptsize{新漢語譯本}"+" & " \
+                        +"\\cellcolor{"+colorArr[0]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
+                # ---------------------------------------------------
+                # add the content of cuv1 to 2nd row
+                # ---------------------------------------------------
                 bibleStr = content_cuv1[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
-                # <<<< when a new version is added, argument in "multirow" requires adjustment >>>>
-                bibleStr1= "\\multirow{10}{*}{\\rotatebox[origin=c]{90}{\\hfill "+words[1]+" "+words[3]+" $"+bibleStr[0]+"$ \\hfill}}" ; fp.write( bibleStr1)
-                # ---------------------------------------------------
-                # add the content of cuv1 to 1st row
-                # ---------------------------------------------------
-                bibleStr2= " & "+"\\cellcolor{"+colorArr[0]+"!"+str(colorIntensity)+"}"+"\\scriptsize{和合本}"+" & "+"\\cellcolor{"+colorArr[0]+"!"+str(colorIntensity)+"}"+bibleStr[1]+" \\\\\n" ; fp.write( bibleStr2)
+                bibleStr = bibleStr[1]
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[1]+"!"+str(colorIntensity)+"}"+"\\scriptsize{和合本}"+" & " \
+                        +"\\cellcolor{"+colorArr[1]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of lzzv to 2nd row
+                # add the content of lzzv to 3rd row
                 # ----------------------------------------------------
                 bibleStr = content_lzzv[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[1]+"!"+str(colorIntensity)+"}"+"\\scriptsize{呂振中本}"+" & "+"\\cellcolor{"+colorArr[1]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[2]+"!"+str(colorIntensity)+"}"+"\\scriptsize{呂振中本}"+" & " \
+                        +"\\cellcolor{"+colorArr[2]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of kjvv to 3rd row
+                # add the content of kjvv to 4th row
                 # ----------------------------------------------------
                 bibleStr = content_kjvv[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[2]+"!"+str(colorIntensity)+"}"+"\\scriptsize{KJV}"+" & "+"\\cellcolor{"+colorArr[2]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr ) ;
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[3]+"!"+str(colorIntensity)+"}"+"\\scriptsize{KJV}"+" & " \
+                        +"\\cellcolor{"+colorArr[3]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr ) ;
                 # ----------------------------------------------------
-                # add the content of cuv2 to 4th row
+                # add the content of cuv2 to 5th row
                 # ----------------------------------------------------
                 bibleStr = content_cuv2[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[3]+"!"+str(colorIntensity)+"}"+"\\scriptsize{和合修定}"+" & "+"\\cellcolor{"+colorArr[3]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[4]+"!"+str(colorIntensity)+"}"+"\\scriptsize{和合修定}"+" & " \
+                        +"\\cellcolor{"+colorArr[4]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of cnvv to 5th row
+                # add the content of cnvv to 6th row
                 # ----------------------------------------------------
                 bibleStr = content_cnvv[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[4]+"!"+str(colorIntensity)+"}"+"\\scriptsize{新譯本}"+" & "+"\\cellcolor{"+colorArr[4]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[5]+"!"+str(colorIntensity)+"}"+"\\scriptsize{新譯本}"+" & " \
+                        +"\\cellcolor{"+colorArr[5]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of nrsv to 6th row
+                # add the content of nrsv to 7th row
                 # ----------------------------------------------------
                 bibleStr = content_nrsv[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[5]+"!"+str(colorIntensity)+"}"+"\\scriptsize{NRSV}"+" & "+"\\cellcolor{"+colorArr[5]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[6]+"!"+str(colorIntensity)+"}"+"\\scriptsize{NRSV}"+" & " \
+                        +"\\cellcolor{"+colorArr[6]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ---------------------------------------------------
-                # add the content of wenl to 7th row
+                # add the content of wenl to 8th row
                 # ---------------------------------------------------
                 bibleStr = content_wenl[sentenceIdx].replace("\n","")
                 bibleStr1 = []
                 for c in bibleStr :
                     if ( not c.isdigit() ) and ( not c == "." ) :
                         bibleStr1.append(c)
-                bibleStr = " & "+"\\cellcolor{"+colorArr[6]+"!"+str(colorIntensity)+"}"+"\\scriptsize{文理本}"+" & "+"\\cellcolor{"+colorArr[6]+"!"+str(colorIntensity)+"}"+''.join(bibleStr1)+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[7]+"!"+str(colorIntensity)+"}"+"\\scriptsize{文理本}"+" & " \
+                        +"\\cellcolor{"+colorArr[7]+"!"+str(colorIntensity)+"}"+''.join(bibleStr1)+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of tcv19 to 8th row
+                # add the content of tcv19 to 9th row
                 # ----------------------------------------------------
                 bibleStr = content_tcv19[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[7]+"!"+str(colorIntensity)+"}"+"\\scriptsize{現中2019}"+" & "+"\\cellcolor{"+colorArr[7]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[8]+"!"+str(colorIntensity)+"}"+"\\scriptsize{現中2019}"+" & " \
+                        +"\\cellcolor{"+colorArr[8]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of msgv to 9th row
+                # add the content of msgv to 10th row
                 # ----------------------------------------------------
                 bibleStr = content_msgv[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = " & "+"\\cellcolor{"+colorArr[8]+"!"+str(colorIntensity)+"}"+"\\scriptsize{Message}"+" & "+"\\cellcolor{"+colorArr[8]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[9]+"!"+str(colorIntensity)+"}"+"\\scriptsize{Message}"+" & " \
+                        +"\\cellcolor{"+colorArr[9]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ----------------------------------------------------
-                # add the content of sblgnt to 10th row
+                # add the content of sblgnt to 11th row
                 # ----------------------------------------------------
                 bibleStr = content_sblgnt[sentenceIdx].replace("\n","")
                 bibleStr = bibleStr.split(" ",1)
                 bibleStr = bibleStr[1]
-                bibleStr = bibleStr.replace("⸂","’")
-                bibleStr = bibleStr.replace("⸃","‘")
-                bibleStr = bibleStr.replace("⸀","「")
-                bibleStr = bibleStr.replace("⸁","「˙")
-                bibleStr = bibleStr.replace("⸄","’˙")
-                bibleStr = bibleStr.replace("⸅","˙‘")
-                bibleStr = " & "+"\\cellcolor{"+colorArr[9]+"!"+str(colorIntensity)+"}"+"\\scriptsize{希臘原文}"+" & "+"\\cellcolor{"+colorArr[9]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n" ; fp.write( bibleStr )
+                #bibleStr = bibleStr.replace("⸂","’")
+                #bibleStr = bibleStr.replace("⸃","‘")
+                #bibleStr = bibleStr.replace("⸀","「")
+                #bibleStr = bibleStr.replace("⸁","「˙")
+                #bibleStr = bibleStr.replace("⸄","’˙")
+                #bibleStr = bibleStr.replace("⸅","˙‘")
+                bibleStr = "\\sblgoodgreek " + bibleStr
+                bibleStr = " & " \
+                        +"\\cellcolor{"+colorArr[10]+"!"+str(colorIntensity)+"}"+"\\scriptsize{希臘原文}"+" & " \
+                        +"\\cellcolor{"+colorArr[10]+"!"+str(colorIntensity)+"}"+bibleStr+" \\\\\n"
+                fp.write( bibleStr )
                 # ---------------------------------------------------
                 # end current sentence
                 # ---------------------------------------------------
